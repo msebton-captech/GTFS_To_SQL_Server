c3b9bdb0c81e931dffb025ea3fb42b136e08784f
msebton-captech <47540179+msebton-captech@users.noreply.github.com>
2024-11-25 15:34:38 -0500

    added index mapping

--- GTFS_Ingest/DatabaseFunctions.cs
+++ GTFS_Ingest/DatabaseFunctions.cs
@@ -14,38 +14,39 @@ class DatabaseFunctions
             // Opening the database connection
             connection.Open();
 
+            // Dictionary to map keys to their respective indices
+            Dictionary<string, int> keyIndexMap = new Dictionary<string, int>
+            {
+                { "route_id", 5 },
+                { "agency_id", 4 },
+                { "route_short_name", 8 },
+                { "route_long_name", 0 },
+                { "route_desc", 6 },
+                { "route_type", 1 },
+                { "route_url", 7 },
+                { "route_color", 3 },
+                { "route_text_color", 2 }
+            };
+
             // Iterating through each set of data in newData
             foreach (List<string> values in newData)
             {
                 // Checking if the data already exists in the database
-                if (!IsDataExists(connection, values, selectString))
+                if (!IsDataExists(connection, values, selectString, keyIndexMap))
                 {
                     // If data does not exist, preparing to insert it into the database
                     using (SqlCommand insertCommand = new SqlCommand(insertString, connection))
                     {
                         // Adding parameters for insertion
-                        
-                        // israel
-                        //insertCommand.Parameters.AddWithValue("@Value1", values[0]);
-                        //insertCommand.Parameters.AddWithValue("@Value2", values[1]);
-                        //insertCommand.Parameters.AddWithValue("@Value3", values[2]);
-                        //insertCommand.Parameters.AddWithValue("@Value4", values[3]);
-                        //insertCommand.Parameters.AddWithValue("@Value5", values[4]);
-                        //insertCommand.Parameters.AddWithValue("@Value6", values[5]);
-                        //insertCommand.Parameters.AddWithValue("@Value7", values[6]);
-                        //insertCommand.Parameters.AddWithValue("@Value8", "FFFFFF");
-                        //insertCommand.Parameters.AddWithValue("@Value9", 0);
-
-                        // metro
-                        insertCommand.Parameters.AddWithValue("@Value1", values[5]);
-                        insertCommand.Parameters.AddWithValue("@Value2", values[4]);
-                        insertCommand.Parameters.AddWithValue("@Value3", values[8]);
-                        insertCommand.Parameters.AddWithValue("@Value4", values[0]);
-                        insertCommand.Parameters.AddWithValue("@Value5", values[6]);
-                        insertCommand.Parameters.AddWithValue("@Value6", values[1]);
-                        insertCommand.Parameters.AddWithValue("@Value7", values[7]);
-                        insertCommand.Parameters.AddWithValue("@Value8", values[3]);
-                        insertCommand.Parameters.AddWithValue("@Value9", values[2]);
+                        insertCommand.Parameters.AddWithValue("@Value1", values[keyIndexMap["route_id"]]);
+                        insertCommand.Parameters.AddWithValue("@Value2", values[keyIndexMap["agency_id"]]);
+                        insertCommand.Parameters.AddWithValue("@Value3", values[keyIndexMap["route_short_name"]]);
+                        insertCommand.Parameters.AddWithValue("@Value4", values[keyIndexMap["route_long_name"]]);
+                        insertCommand.Parameters.AddWithValue("@Value5", values[keyIndexMap["route_desc"]]);
+                        insertCommand.Parameters.AddWithValue("@Value6", values[keyIndexMap["route_type"]]);
+                        insertCommand.Parameters.AddWithValue("@Value7", values[keyIndexMap["route_url"]]);
+                        insertCommand.Parameters.AddWithValue("@Value8", values[keyIndexMap["route_color"]]);
+                        insertCommand.Parameters.AddWithValue("@Value9", values[keyIndexMap["route_text_color"]]);
 
                         // Executing the insertion command
                         insertCommand.ExecuteNonQuery();
@@ -61,22 +62,15 @@ class DatabaseFunctions
     }
 
     // Method to check if data already exists in the database
-    public static bool IsDataExists(SqlConnection connection, List<string> values, string selectString)
+    public static bool IsDataExists(SqlConnection connection, List<string> values, string selectString, Dictionary<string, int> keyIndexMap)
     {
         // Executing a SELECT query to check for existing data
         using (SqlCommand selectCommand = new SqlCommand(selectString, connection))
         {
             // Adding parameters to the SELECT query
-
-            // israel
-            //selectCommand.Parameters.AddWithValue("@Value1", values[0]);
-            //selectCommand.Parameters.AddWithValue("@Value2", values[1]);
-            //selectCommand.Parameters.AddWithValue("@Value3", values[2]);
-
-            // metro
-            selectCommand.Parameters.AddWithValue("@Value1", values[5]);
-            selectCommand.Parameters.AddWithValue("@Value2", values[4]);
-            selectCommand.Parameters.AddWithValue("@Value3", values[0]);
+            selectCommand.Parameters.AddWithValue("@Value1", values[keyIndexMap["route_id"]]);
+            selectCommand.Parameters.AddWithValue("@Value2", values[keyIndexMap["agency_id"]]);
+            selectCommand.Parameters.AddWithValue("@Value3", values[keyIndexMap["route_long_name"]]);
 
             // Executing the SELECT query and retrieving the count of matching records
             int count = (int)selectCommand.ExecuteScalar();
